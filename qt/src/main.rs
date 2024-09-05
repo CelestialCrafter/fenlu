9955a15631c06eec7d06ec11befb020494a1ebbc
CelestialCrafter <celestialcraftermc@gmail.com>
2024-09-05 10:48:48 -0500

    fix proxy transform

--- qt/src/main.rs
+++ qt/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut engine = QQmlApplicationEngine::new();
 
     if let Some(engine) = engine.as_mut() {
-        engine.load(&QUrl::from("qrc:/qt/qml/io.github/CelestialCrafter/fenlu/src/qml/media.qml"));
+        engine.load(&QUrl::from("qrc:/qt/qml/io/github/CelestialCrafter/fenlu/src/qml/media.qml"));
     }
 
     if let Some(app) = app.as_mut() {
