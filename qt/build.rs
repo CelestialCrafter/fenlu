4f93b8f9af27633002d0e1a62ba850638d78a5f1
CelestialCrafter <celestialcraftermc@gmail.com>
2024-09-05 16:44:44 -0500

    toml qobject

--- qt/build.rs
+++ qt/build.rs
@@ -5,7 +5,7 @@ fn main() {
         .qt_module("Network")
         .qml_module(QmlModule {
             uri: "io.github.CelestialCrafter.fenlu",
-            rust_files: &["src/objects/media.rs"],
+            rust_files: &["src/objects/media.rs", "src/objects/toml.rs"],
             qml_files: &["src/qml/media.qml", "src/qml/CustomScrollGridView.qml"],
             ..Default::default()
         })
--- qt/src/objects/media.rs
+++ qt/src/objects/media.rs
@@ -1,6 +1,5 @@
 #[cxx_qt::bridge]
 pub mod qobject {
-
     unsafe extern "C++" {
         include!("cxx-qt-lib/qstring.h");
         type QString = cxx_qt_lib::QString;
