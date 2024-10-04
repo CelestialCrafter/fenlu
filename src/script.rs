7d32718edf40179bc5c299f2f44a3fa1a56decbf
CelestialCrafter <celestialcraftermc@gmail.com>
2024-10-03 20:49:50 -0500

    wip

--- src/script.rs
+++ src/script.rs
@@ -5,8 +5,9 @@ use eyre::{eyre, OptionExt, Report, Result};
 use futures::future::join_all;
 use tokio::{sync::{mpsc, oneshot}, task};
 
-use crate::{protocol::{capabilities::Capabilities, media::Media, messages::{Id, Request, Response}}, utils::{self, child_guard::ChildGuard}};
+use crate::{protocol::{capabilities::{self, Capabilities}, messages::{Id, Request, Response}}, utils::generate_id};
 
+#[derive(Debug)]
 pub struct Script {
     pub path: PathBuf,
     pub capabilities: Capabilities,
@@ -15,17 +16,18 @@ pub struct Script {
 }
 
 impl Script {
-    pub async fn request(&self, req: Request) -> Result<Response> {
+    pub async fn request(&self, req: Request) -> Response {
         let (tx, rx) = oneshot::channel();
-        if let Some(_) = self.pending_requests.insert(req.id, tx) {
+        if let Some(_) = self.pending_requests.insert(req.id.clone(), tx) {
             panic!("request was already in pending map");
         }
 
-        rx.await.map_err(|_| eyre!("receiver was dropped"))
+        self.request_tx.send(req).await.expect("reciever was dropped");
+        rx.await.expect("sender was dropped")
     }
 }
 
-pub fn spawn_server(path: PathBuf) -> Result<Arc<Script>> {
+pub async fn spawn_server(path: PathBuf) -> Result<Arc<Script>> {
     let path = path.canonicalize()?;
 
     let mut dir = path.clone();
@@ -40,37 +42,63 @@ pub fn spawn_server(path: PathBuf) -> Result<Arc<Script>> {
     let mut stdout = BufReader::new(child.stdout.take().ok_or_eyre("could not take stdout")?);
     let mut stdin = BufWriter::new(child.stdin.take().ok_or_eyre("could not take stdout")?);
 
-    let _guard = ChildGuard(child);
-
-    let mut capabilities = String::new();
-    stdout.read_line(&mut capabilities)?;
-    let capabilities = serde_json::from_str(capabilities.as_str())?;
-
-    let bs = 4;
-    let (request_tx, mut request_rx) = mpsc::channel(bs);
-    let script = Arc::new(Script {
+    let (request_tx, mut request_rx) = mpsc::channel(4);
+    let mut script = Script {
         path,
-        capabilities,
+        capabilities: Capabilities::default(),
         request_tx,
         pending_requests: DashMap::new()
-    });
+    };
 
+    // manually request capabilities before the script is wrapped in an Arc and therefore immutable
+    {
+        let id = generate_id();
+
+        let mut encoded = serde_json::to_vec(&Request {
+            id: id.clone(),
+            method: capabilities::CAPABILITIES_METHOD.to_string(),
+            params: serde_json::to_value(capabilities::CapabilitiesRequest {})?
+        })?;
+        encoded.push(b'\n');
+
+        stdin.write_all(&encoded)?;
+        stdin.flush()?;
+
+        let mut response = String::new();
+        stdout.read_line(&mut response)?;
+        let response: Response = serde_json::from_str(&response)?;
+
+        assert!(id == response.id, "initial response id was not the same as capabilities request id");
+        script.capabilities = serde_json::from_value(response.result()?)?;
+    }
+
+    let script = Arc::new(script);
+
+    // take incomming requests and send them to the script
     let request_handle = task::spawn(async move {
         while let Some(request) = request_rx.recv().await {
-            let encoded = serde_json::to_vec(&request)?;
+            let mut encoded = serde_json::to_vec(&request)?;
+            encoded.push(b'\n');
             stdin.write_all(&encoded)?;
+            stdin.flush()?;
+            println!("-> {}", request.id);
         }
 
         Ok::<_, Report>(())
     });
 
+    // take incomming responses
+    // remove them from pending requests
+    // send them to the pending request tx
     let response_handle = {
         let script = script.clone();
         task::spawn(async move {
             for line in stdout.lines() {
                 let response: Response = serde_json::from_str(line?.as_str())?;
+                let id = response.id.clone();
                 if let Some((_, tx)) = script.pending_requests.remove(&response.id) {
                     tx.send(response).map_err(|_| eyre!("receiver was dropped"))?;
+                    println!("<- {}", id);
                 }
             }
 
@@ -93,10 +121,5 @@ pub fn spawn_server(path: PathBuf) -> Result<Arc<Script>> {
     });
 
     Ok(script)
-}
-
-pub fn append_history(path: PathBuf, mut media: Media) {
-    let name = utils::path_to_name(&path);
-    media.history.push(name);
 }
 
