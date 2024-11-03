328b49b2bf25f21062e116d96f56a47176d9f96a
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-03 10:09:31 -0600

    filter nodes && filter-tags && remove unneccesary stuff from source-pixiv

---
+++ nodes/filter-tags.py
@@ -0,0 +1,30 @@
+from common import listen
+
+config = {}
+
+def has(a, b):
+    return any([tag in a for tag in b])
+
+def filter(media):
+    if 'tags' not in media['extraMetadata']:
+        return True
+
+    tags = media['extraMetadata']['tags']
+    return has(config['included'], tags) and not has(config['excluded'], tags)
+
+def handle_filter(params):
+    return [filter(media) for media in params]
+
+def handle_initialize(params):
+    global config
+    config = params['config']
+
+    return {
+        'version': "95a247050de65c132541eabe3d93ca0b7c9b5a65",
+        'capabilities':  ["media/filter"]
+    }
+
+listen({
+    'initialize/initialize': handle_initialize,
+    'media/filter': handle_filter,
+})
