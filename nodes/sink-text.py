4191578c51126ad3cc82a36d6bbf000ff4a696df
CelestialCrafter <celestialcraftermc@gmail.com>
2024-11-06 12:14:54 -0600

    repurpose sink-print as file output

--- nodes/sink-text.py
+++ nodes/sink-text.py
@@ -1,12 +1,15 @@
 import json
 
-from common import listen, log
+from common import listen
 
 def handle_sink(params):
     for media in params:
-        log(json.dumps(media))
+        file.write(json.dumps(media) + '\n')
 
-def handle_initialize(_):
+def handle_initialize(params):
+    global file
+    file = open(params['config']['path'], 'w+')
+
     return {
         'version': "667430e325dda8b8949276d39b87c031a304c55b",
         'capabilities':  ["media/sink"]
